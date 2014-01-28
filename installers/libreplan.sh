d2e0c8e710d838d31e17acc4f1651fc67c8c69ad
SixArm <sixarm@sixarm.com>
2014-01-28 06:32:20 -0800

    Add LibrePlan project managment app

---
+++ installers/libreplan.sh
@@ -0,0 +1,11 @@
+# LibrePlan open source project planning application
+
+# We may need to install:
+sudo apt-get install python-software-properties
+
+# In Ubuntu 12.10 we need to install:
+sudo apt-get install software-properties-common
+
+sudo add-apt-repository ppa:libreplan/ppa
+sudo apt-get update
+sudo apt-get install libreplan
